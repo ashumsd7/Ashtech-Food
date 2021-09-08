33edb2a6b166a4ad9de86d5ac90995defaa5862e
ashumsd7 <aat8001@gmail.com>
2021-09-09 02:57:23 +0530

    navbar & logos added

---
+++ src/components/boss/TheNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+      <nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="../../assets/logo.png" alt="" srcset=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- <span class="navbar-toggler-icon"></span> -->
+      <img src="../../assets/menu.png" alt="" srcset="">
+      <!-- //Your ICON -->
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Get Foods</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reviews</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">FAQs</a>
+        </li>
+        <li class="nav-item">
+          <button class=" btn btn-success main-btn">+123 456789</button>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+  </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-   <a href="" class="btn btn-primary">hello</a>
+  <the-navbar></the-navbar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TheNavbar from '../components/boss/TheNavbar.vue'
 
 
 export default {
   name: 'Home',
+  components:{
+    TheNavbar
+  }
 
 }
 </script>
